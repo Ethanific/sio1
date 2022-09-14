43872ca4e2a8e53e6b270d362665cba457fa27a1
thfruchart <66477650+thfruchart@users.noreply.github.com>
2022-09-14 11:58:57 +0200

    Add files via upload

---
+++ TP2Exo2_aide.py
@@ -0,0 +1,17 @@
+﻿from random import *
+
+nb_essai = 4  # valeur réelle : 10
+MAX = 15      # valeur réelle : 1000
+
+resultat = randint(1,MAX)
+print(resultat) #utile pour les tests
+compteur=0
+print('Un nombre entre 1 et', MAX, 'est choisi : vous avez', nb_essai, 'essais pour le deviner')
+
+gagne = False #initialement, le joueur n'a pas (encore) gagné
+
+
+while not gagne :
+    message = 'Essai n°' + str(compteur) + ' : '
+    n=int(input(message))
+    
