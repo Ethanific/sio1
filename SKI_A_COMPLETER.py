fdb4a72b3fbbe0d6d0b130175c2cac35614fd363
thfruchart <66477650+thfruchart@users.noreply.github.com>
2023-03-16 09:37:20 +0100

    Add files via upload

---
+++ SKI_A_COMPLETER.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+
+pistes=[['A','B','B','C','C','D','D','E','F'],
+        ['B','C','E','A','D','A','E','F','D']]
+
+parcours1='BEF'
+parcours2='ABCDFDA'
+parcours3='DABEFDA'
+
+
+def piste_ouverte(debut, fin):
+    valide=False
+    for i in range(9):
+        if pistes[0][i]==debut and pistes[1][i]==fin:
+            valide=True
+    return valide
+
+def parcours_valide(parcours):
+    resu = True
+    for i in range(len(parcours)):
+        deb = parcours[i]
+        fin = parcours [i+1]
+        print(deb, fin)
+        if piste_ouverte(deb, fin= ==False):
+            resu = False
+    return resu
+
+
+
